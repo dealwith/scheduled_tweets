96b78002e70bfdc6a17ec4ec1d153e890be8a9b1
dealwith <gleb.krishin@icloud.com>
2023-04-10 16:35:57 +0200

    Part 14: Handling Sign Up Errors

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -8,4 +8,20 @@ class RegistrationsController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to root_path, notice: 'Successfully created account!'
+    else
+      flash.now[:alert] = @user.errors.full_messages.join(', ')
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
 end
