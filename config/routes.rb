2933c972b70751c9b78fd1bb2d7cc0152729b5b3
dealwith <gleb.krishin@icloud.com>
2023-04-15 17:55:57 +0200

    Part 18: New Navbar

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   get 'sign_up', to: 'registrations#new'
   post 'sign_up', to: 'registrations#create'
-  delete 'logout', to: "sessions#destroy"
+  delete 'logout', to: 'sessions#destroy'
 
   get 'sign_in', to: 'sessions#new'
   post 'sign_in', to: 'sessions#create'
